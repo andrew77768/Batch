97d906ac79565f1facd36fc342b38eda6c261d58
Andrew <andrew.2008@live.co.uk>
2019-02-13 16:21:25 +0000

    Update ITInfo.bat
    Added serial number support

--- ITInfo.bat
+++ ITInfo.bat
@@ -12,6 +12,9 @@ ipconfig | findstr IPv4 >> %DIR%%SUBFILENAME%
 ipconfig | findstr IPv6 >> %DIR%%SUBFILENAME%
 echo. >> %DIR%%SUBFILENAME%
 
+wmic bios get serialnumber >> %DIR%%SUBFILENAME% 
+echo.   >> %DIR%%SUBFILENAME%.
+
 if exist "C:\Program Files\Microsoft Office\Office16\" cscript "C:\Program Files\Microsoft Office\Office16\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAME%
 if exist "C:\Program Files\Microsoft Office\Office15\" cscript "C:\Program Files\Microsoft Office\Office15\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAME%
 if exist "C:\Program Files\Microsoft Office\Office14\" cscript "C:\Program Files\Microsoft Office\Office14\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAME%
@@ -23,6 +26,3 @@ if exist "C:\Program Files (x86)\Microsoft Office\Office15\" cscript "C:\Program
 if exist "C:\Program Files (x86)\Microsoft Office\Office14\" cscript "C:\Program Files (x86)\Microsoft Office\Office14\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAME%
 if exist "C:\Program Files (x86)\Microsoft Office\Office12\" cscript "C:\Program Files (x86)\Microsoft Office\Office12\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAME%
 if exist "C:\Program Files (x86)\Microsoft Office\Office11\" cscript "C:\Program Files (x86)\Microsoft Office\Office11\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAME%
-
-
-pause
