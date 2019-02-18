9b78d2ca72d7a09dca96ca7d7d3ad5d1cc795a19
Andrew <andrew.2008@live.co.uk>
2019-02-18 07:14:04 +0000

    Update ITInfo.bat
    Changes how the program works, everything is dumped to a temp file, once done findstr goes through and removes all white space and pipes it through to the output file and then it proceeds to delete the temp file

--- ITInfo.bat
+++ ITInfo.bat
@@ -1,31 +1,36 @@
 REM Change this to change the save location of the file
 set DIR=S:\ITInfo\
 REM Change this to change the file name
+set SUBFILENAMETEMP=%ComputerName%_ITInfo_Temp.txt
 set SUBFILENAME=%ComputerName%_ITInfo.txt
 
 REM Don't touch below
-if exist "%DIR%%SUBFILENAME%" exit
+if exist "%DIR%%SUBFILENAMETEMP%" exit
 
-whoami >> %DIR%%SUBFILENAME%
-REM systeminfo | find "System Boot Time:" >> %DIR%%SUBFILENAME%
-echo. >> %DIR%%SUBFILENAME%
-echo Computer Name: %ComputerName% >> %DIR%%SUBFILENAME%
-echo.   >> %DIR%%SUBFILENAME%.
-ipconfig | findstr IPv4 >> %DIR%%SUBFILENAME%
-ipconfig | findstr IPv6 >> %DIR%%SUBFILENAME%
-echo. >> %DIR%%SUBFILENAME%
-wmic bios get name,serialnumber,version /value |more >> %DIR%%SUBFILENAME% 
-wmic csproduct get name,identifyingnumber,uuid /value |more >> %DIR%%SUBFILENAME% 
-wmic cpu get name,CurrentClockSpeed,MaxClockSpeed /value |more >> %DIR%%SUBFILENAME% 
-echo.   >> %DIR%%SUBFILENAME%.
+whoami >> %DIR%%SUBFILENAMETEMP%
+REM systeminfo | find "System Boot Time:" >> %DIR%%SUBFILENAMETEMP%
+echo. >> %DIR%%SUBFILENAMETEMP%
+echo Computer Name: %ComputerName% >> %DIR%%SUBFILENAMETEMP%
+echo.   >> %DIR%%SUBFILENAMETEMP%.
+ipconfig | findstr IPv4 >> %DIR%%SUBFILENAMETEMP%
+ipconfig | findstr IPv6 >> %DIR%%SUBFILENAMETEMP%
+echo. >> %DIR%%SUBFILENAMETEMP%
+wmic bios get name,serialnumber,version /value | more >> %DIR%%SUBFILENAMETEMP% 
+wmic csproduct get name,identifyingnumber,uuid /value | more >> %DIR%%SUBFILENAMETEMP% 
+wmic cpu get name,CurrentClockSpeed,MaxClockSpeed /value | more >> %DIR%%SUBFILENAMETEMP% 
+echo.   >> %DIR%%SUBFILENAMETEMP%.
 
-if exist "C:\Program Files\Microsoft Office\Office16\" cscript "C:\Program Files\Microsoft Office\Office16\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAME%
-if exist "C:\Program Files\Microsoft Office\Office15\" cscript "C:\Program Files\Microsoft Office\Office15\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAME%
-if exist "C:\Program Files\Microsoft Office\Office14\" cscript "C:\Program Files\Microsoft Office\Office14\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAME%
-if exist "C:\Program Files\Microsoft Office\Office12\" cscript "C:\Program Files\Microsoft Office\Office12\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAME%
-if exist "C:\Program Files\Microsoft Office\Office11\" cscript "C:\Program Files\Microsoft Office\Office11\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAME%
-if exist "C:\Program Files (x86)\Microsoft Office\Office16\" cscript "C:\Program Files (x86)\Microsoft Office\Office16\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAME%
-if exist "C:\Program Files (x86)\Microsoft Office\Office15\" cscript "C:\Program Files (x86)\Microsoft Office\Office15\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAME%
-if exist "C:\Program Files (x86)\Microsoft Office\Office14\" cscript "C:\Program Files (x86)\Microsoft Office\Office14\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAME%
-if exist "C:\Program Files (x86)\Microsoft Office\Office12\" cscript "C:\Program Files (x86)\Microsoft Office\Office12\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAME%
-if exist "C:\Program Files (x86)\Microsoft Office\Office11\" cscript "C:\Program Files (x86)\Microsoft Office\Office11\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAME%
+if exist "C:\Program Files\Microsoft Office\Office16\" cscript "C:\Program Files\Microsoft Office\Office16\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAMETEMP%
+if exist "C:\Program Files\Microsoft Office\Office15\" cscript "C:\Program Files\Microsoft Office\Office15\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAMETEMP%
+if exist "C:\Program Files\Microsoft Office\Office14\" cscript "C:\Program Files\Microsoft Office\Office14\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAMETEMP%
+if exist "C:\Program Files\Microsoft Office\Office12\" cscript "C:\Program Files\Microsoft Office\Office12\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAMETEMP%
+if exist "C:\Program Files\Microsoft Office\Office11\" cscript "C:\Program Files\Microsoft Office\Office11\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAMETEMP%
+if exist "C:\Program Files (x86)\Microsoft Office\Office16\" cscript "C:\Program Files (x86)\Microsoft Office\Office16\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAMETEMP%
+if exist "C:\Program Files (x86)\Microsoft Office\Office15\" cscript "C:\Program Files (x86)\Microsoft Office\Office15\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAMETEMP%
+if exist "C:\Program Files (x86)\Microsoft Office\Office14\" cscript "C:\Program Files (x86)\Microsoft Office\Office14\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAMETEMP%
+if exist "C:\Program Files (x86)\Microsoft Office\Office12\" cscript "C:\Program Files (x86)\Microsoft Office\Office12\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAMETEMP%
+if exist "C:\Program Files (x86)\Microsoft Office\Office11\" cscript "C:\Program Files (x86)\Microsoft Office\Office11\OSPP.VBS" /dstatus  | findstr /C:"Last" /C:"LICENSE NAME"  >> %DIR%%SUBFILENAMETEMP%
+
+findstr /v /r /c:"^$" /c:"^[\ \	]*$" "%DIR%%SUBFILENAMETEMP%" >> "%DIR%%SUBFILENAME%"
+
+del "%DIR%%SUBFILENAMETEMP%"
